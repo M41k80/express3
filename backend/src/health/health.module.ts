ff804caf24a02e227a69bca28227bccaf3d37d2e
Agata <agata.morales92@gmail.com>
2025-04-30 18:08:58 -0300

    arreglo import User en health

--- backend/src/health/health.module.ts
+++ backend/src/health/health.module.ts
@@ -4,10 +4,13 @@ import { HealthProfile } from './entities/health-profile.entity';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 import { UsersModule } from '../users/users.module';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([HealthProfile]),
-    UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([HealthProfile, User]), 
+    UsersModule,
+  ],
   providers: [HealthService],
   controllers: [HealthController],
   exports: [HealthService],
