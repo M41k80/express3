262bcb2ca48b910335ba4ddcd02dad4db548c5c1
Magdiel Mora <m41k80@icloud.com>
2025-05-12 18:07:47 -0500

    metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { AuthProvider } from '@/app/context/AuthContext';
 import { Geist, Geist_Mono, Nunito, Lato } from 'next/font/google';
 import './globals.css';
 // import { image } from 'html2canvas/dist/types/css/types/image';
+import type { Metadata } from 'next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,31 +26,32 @@ const lato = Lato({
   weight: ['300', '400', '700', '900'],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'BalanceIA',
-  description: 'BalanceIA es una plataforma inteligente diseñada para ayudarte a llevar una vida saludable y equilibrada, combinando alimentación personalizada y rutinas de ejercicio basadas en inteligencia artificial.',
+  description: 'BalanceIA es una plataforma inteligente...',
   keywords: ['BalanceIA', 'salud', 'alimentación saludable', 'ejercicio', 'inteligencia artificial', 'bienestar', 'vida equilibrada'],
   authors: [{ name: 'BalanceIA', url: 'https://balanceia.com' }],
   openGraph: {
     title: 'BalanceIA',
-    description: 'Transforma tu salud con inteligencia artificial: alimentación personalizada y entrenamiento adaptado a ti.',
+    description: 'Transforma tu salud con inteligencia artificial...',
     url: 'https://balanceia.com',
     siteName: 'BalanceIA',
     locale: 'es_ES',
     type: 'website',
     images: [
       {
-        url: '/Logo-Balance-IA.svg',
+        url: '/Logo-Balance-IA.png',
         width: 1200,
         height: 630,
         alt: 'BalanceIA Logo',
-        type: 'image/svg+xml',
+        type: 'image/png',
       },
     ],
   },
+};
+
 
   
-};
 
 
 export default function RootLayout({
